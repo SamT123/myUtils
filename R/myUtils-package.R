e6e3befef1aea1f06ba76cb93cdea36286c956b4
SamT123 <samturner0306@gmail.com>
2025-11-03 13:31:50 +0000

    Update NAMESPACE

--- R/myUtils-package.R
+++ R/myUtils-package.R
@@ -3,7 +3,6 @@
 
 ## usethis namespace: start
 #' @importFrom seqUtils fast_fasta
-#' @importFrom seqUtils fast_fasta.write
 #' @importFrom seqUtils write_fast_fasta
 ## usethis namespace: end
 NULL
